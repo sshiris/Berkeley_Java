b4b004cc0a835d902b57f434a7f07ba862bb6117
Iris <148431646+iris915520@users.noreply.github.com>
2025-07-14 16:07:25 +0300

    List exercises finished

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,29 +1,50 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
     public static int sum(List<Integer> L) {
-        // TODO: Fill in this function.
-        return 0;
+        int total = 0;
+        for (int l : L){
+            total += l;
+        }
+        return total;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> even = new ArrayList<>();
+        for(int l : L){
+            if(l%2 == 0){
+                even.add(l);
+            }
+        }
+        return even;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> l = new ArrayList<>();
+        for(int l1 : L1){
+            if(L2.contains(l1)){
+                l.add(l1);
+            }
+        }
+        return l;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        // TODO: Fill in this function.
-        return 0;
+        int occurrence = 0;
+        for(String word : words){
+            for(int i = 0; i < word.length(); i++){
+                if (word.charAt(i) == c){
+                    occurrence +=1;
+                }
+            }
+        }
+        return occurrence;
     }
 }
