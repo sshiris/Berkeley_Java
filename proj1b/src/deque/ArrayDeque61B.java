976f61e8c43ecb2f90e6a06b1a19af44e1a4881e
Iris <148431646+iris915520@users.noreply.github.com>
2025-09-07 17:14:32 +0300

    addfirst and last

---
+++ proj1b/src/deque/ArrayDeque61B.java
@@ -0,0 +1,66 @@
+package deque;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T>{
+    int size;
+    int frontPosition;
+    int backPosition;
+    private T[] items;
+
+    public ArrayDeque61B(){
+        size = 8;
+        frontPosition = 0;
+        backPosition = 0;
+        items =(T[]) new Object[size];
+    }
+
+    @Override
+    public void addFirst(T x) {
+        items[frontPosition] = x;
+        frontPosition = Math.floorMod(frontPosition-1, size);
+
+    }
+
+    @Override
+    public void addLast(T x) {
+        items[backPosition] = x;
+        backPosition = Math.floorMod(backPosition+1, size);
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
